4031818314c195e66f4687d1b640fa84b2f01f56
ikun <2783669328@qq.com>
2023-02-26 22:51:13 +0800

    test item

--- js/login.js
+++ js/login.js
@@ -1,16 +1,17 @@
 (() => {
-    // 判断客户端
-    // if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-    //     window.location.href = ""; //手机
-    // } else {
-    //     window.location.href = ""; //电脑
-    // }
-    var den = document.querySelector("#den");
-    var input = document.querySelectorAll("input");
-    console.log(input);
-    den.addEventListener('click', () => {
-        if (input[0].value !== "" && input[1].value !== "") {
-            location.replace('./index.html');
-        }
-    })
-})()
+  // 判断客户端
+  // if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+  //     window.location.href = ""; //手机
+  // } else {
+  //     window.location.href = ""; //电脑
+  // }
+  var den = document.querySelector("#den");
+  var input = document.querySelectorAll("input");
+  console.log(input);
+  den.addEventListener("click", () => {
+    if (input[0].value !== "" && input[1].value !== "") {
+      location.replace("./index.html");
+      console.log("你好!今天修改的时间是" + new Date());
+    }
+  });
+})();
